2c0d85b30b238ee50c7d4ca0d5a8d2a17772b48d
Bastian Perez <didier.bpp@gmail.com>
2023-02-12 00:09:10 -0300

    Version 1.0.0-rc ok

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { DigitalSignature } from './components/DigitalSignature';
+import SignaturePad from 'signature_pad';
+export { SignaturePad };
 export default DigitalSignature;
 export * from './interfaces/interfaces';
 
